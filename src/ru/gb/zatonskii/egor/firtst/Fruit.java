088c194382e5da87e2f2100eb4828115f1f942c9
Egor <egor-zatonskii@yandex.ru>
2022-02-17 08:05:49 +0400

    Закончил 1 домашку, но не хватила времени и знаний допилить свою идею о сортировке по цвету

--- src/ru/gb/zatonskii/egor/firtst/Fruit.java
+++ src/ru/gb/zatonskii/egor/firtst/Fruit.java
@@ -1,14 +1,19 @@
 package ru.gb.zatonskii.egor.firtst;
 
 public class Fruit {
-    public static float getWeightBox;
     private float WeightFruit;
+    private String ColourFruit; //В задании нет речи про цвет. Это для себя и пока что не имею понятия, что буду с ним делать по итогу//
 
-    public Fruit(float WeightFruit) {
+    public Fruit(float WeightFruit, String ColourFruit) {
         this.WeightFruit = WeightFruit;
+        this.ColourFruit = ColourFruit;
     }
 
     public float getWeightFruit() {
         return WeightFruit;
+    }
+
+    public String getColourFruit() {
+        return ColourFruit;
     }
 }
--- src/ru/gb/zatonskii/egor/firtst/Orange.java
+++ src/ru/gb/zatonskii/egor/firtst/Orange.java
@@ -1,7 +1,12 @@
 package ru.gb.zatonskii.egor.firtst;
 
 public class Orange extends Fruit{
-    public Orange(float WeightFruit) {
-        super(WeightFruit);
+    // принимаем вес и цвет
+    public Orange(float WeightFruit,String ColourFruit) {
+        // инициализируем их эначения
+        super(WeightFruit, ColourFruit);
     }
 }
+
+// пометки оставляю для себя, чтобы понимать цепочку действий.
+// говорят, перед тем как писать код, нужно расписать план действий. Вот с этим проблемы.
